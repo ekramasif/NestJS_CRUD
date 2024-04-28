c4d4a530480f3eaddf6527aa2acea698a5775bc2
ekramasif <mdekramuddin23@gmail.com>
2024-04-29 01:21:58 +0600

    Auth updated

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { HttpModule } from '@nestjs/axios'; // Ensure HttpModule is imported
+import { HttpModule } from '@nestjs/axios';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), HttpModule], // Include HttpModule
+  imports: [TypeOrmModule.forFeature([User]), HttpModule],
   providers: [UsersService],
   controllers: [UsersController],
 })
